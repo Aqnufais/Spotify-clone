89bd0c26822e136d973ab0ed341ca60449fe2de1
aqstin <aqnufais45@gmail.com>
2025-02-16 18:03:06 +0530

    project setup

---
+++ src/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Login.css"
+
+function Login() {
+    return (
+        <>
+        <div className="login">
+            <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Full_Logo_RGB_Green.png" alt="Spotify_logo" />
+            <a href="#">Login with Spotify</a>
+        </div>
+        </>
+
+    )
+}
+
+export default Login
